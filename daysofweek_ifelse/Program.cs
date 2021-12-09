e2ee1095df518b3617966056ee8212e56b8fee2e
e d u a r d a <86473064+eduardarn@users.noreply.github.com>
2021-12-09 12:46:22 -0300

    Add files via upload

---
+++ daysofweek_ifelse/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace daysofweek_ifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dia = DateTime.Now.DayOfWeek;
+
+            if (dia == DayOfWeek.Sunday)
+            {
+                Console.WriteLine($"Today is: {DayOfWeek.Sunday}");
+            }
+            else if (dia == DayOfWeek.Monday)
+            {
+                Console.WriteLine($"Today is: {DayOfWeek.Monday}");
+            }
+            else if (dia == DayOfWeek.Tuesday)
+            {
+                Console.WriteLine($"Today is: {DayOfWeek.Tuesday}");
+            }
+            else if (dia == DayOfWeek.Wednesday)
+            {
+                Console.WriteLine($"Today is: {DayOfWeek.Wednesday}");
+            }
+            else if (dia == DayOfWeek.Thursday)
+            {
+                Console.WriteLine($"Today is: {DayOfWeek.Thursday}");
+            }
+            else if (dia == DayOfWeek.Friday)
+            {
+                Console.WriteLine($"Today is: {DayOfWeek.Friday}");
+            }
+            else if (dia == DayOfWeek.Saturday)
+            {
+                Console.WriteLine($"Today is: {DayOfWeek.Saturday}");
+            }
+         
+        }
+    }
+}
